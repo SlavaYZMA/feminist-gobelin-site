9cd67b56497ebc3549523f7cd5412dbdce429f54
SlavaYZMA <slavasolen@gmail.com>
2025-08-14 13:38:31 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'dist',
-    },
-    base: '/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  base: '/',
+  css: {
+    preprocessorOptions: {
+      css: {
+        charset: false // Убедитесь, что кодировка не вызывает проблем
+      }
+    }
+  }
 });
