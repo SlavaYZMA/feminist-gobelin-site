78c4a6ea8b5957c64300304ca331b2a887d9d739
SlavaYZMA <slavasolen@gmail.com>
2025-08-15 10:46:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  base: '/',
-  css: {
-    preprocessorOptions: {
-      css: {
-        charset: false // Убедитесь, что кодировка не вызывает проблем
-      }
-    }
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+    loader: 'jsx'
   }
 });
